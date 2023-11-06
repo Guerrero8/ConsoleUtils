bd82c019ee5ee5dafa5ae287d25c93ede99810c0
Guerrero8 <igo2rlol@gmail.com>
2023-11-06 19:18:53 +0300

    print < println

--- src/main/java/org/AdiosPayasos/ConsoleUtils.java
+++ src/main/java/org/AdiosPayasos/ConsoleUtils.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 public class ConsoleUtils {
     public static void setPlayerNickname() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("ник дай:\n");
+        System.out.println("ник дай:\n");
         String nickname = scanner.nextLine();
         System.out.println("ник на:\n" + nickname);
     }
     public static void setPlayerLogin() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("логин дай:\n");
+        System.out.println("логин дай:\n");
         String nickname = scanner.nextLine();
         System.out.println("логин на:\n" + nickname);
     }
     public static void setPlayerPasword() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("пороль дай:\n");
+        System.out.println("пороль дай:\n");
         String nickname = scanner.nextLine();
         System.out.println("пороль на:\n" + nickname);
     }
